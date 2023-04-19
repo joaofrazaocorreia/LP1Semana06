efc178f4d42f3612287ba78da1663b44826d361e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-19 13:36:21 +0100

    Test PickupPowerUp and TakeDamage and print values

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -13,14 +13,48 @@ namespace GameSix
 
             for (int i = 0; i < foeCount; i++)
             {
-                Console.Write($"Name of foe num. {i+1}:");
+                Console.Write($"Name of foe num. {i+1}: ");
                 myFoes[i] = new Foe(Console.ReadLine());
             }
 
+            Console.WriteLine("");
             foreach (Foe f in myFoes)
             {
                 Console.WriteLine(f.GetName());
             }
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()}'s stats:");
+            Console.WriteLine($"{myFoes[0].GetHealth()} Health, "+
+                                $"{myFoes[0].GetShield()} Shield");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()} takes 50.5 damage!");
+            myFoes[0].TakeDamage(50.5f);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()}'s stats:");
+            Console.WriteLine($"{myFoes[0].GetHealth()} Health, "+
+                                $"{myFoes[0].GetShield()} Shield");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()} gets 4.2 shield!");
+            myFoes[0].PickupPowerUp(PowerUp.Shield, 4.2f);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()}'s stats:");
+            Console.WriteLine($"{myFoes[0].GetHealth()} Health, "+
+                                $"{myFoes[0].GetShield()} Shield");
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()} heals 80 health!");
+            myFoes[0].PickupPowerUp(PowerUp.Health, 80f);
+
+            Console.WriteLine("");
+            Console.WriteLine($"Foe {myFoes[0].GetName()}'s stats:");
+            Console.WriteLine($"{myFoes[0].GetHealth()} Health, "+
+                                $"{myFoes[0].GetShield()} Shield");
+
         }
     }
 }
