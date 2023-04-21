8c1caf7ef864fe917d6497aaf50064d88e7e3bc5
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-04-21 13:18:34 +0100

    Create project GameSixFriday

---
+++ GameSixFriday/Foe.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameSixFriday
+{
+    public class Foe
+    {
+        private string name;
+        private float health;
+        private float shield;
+
+        private static int powerupAmount;
+
+        /// Construtor
+        public Foe(string name)
+        {
+            SetName(name);
+            health = 100;
+            shield = 0;
+            powerupAmount = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public static int GetPowerupAmount()
+        {
+            return powerupAmount;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name.Trim();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStilltoInflict = -shield;
+                shield = 0;
+                health -= damageStilltoInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        public void PickupPowerUp(PowerUp powerup, float amount)
+        {
+            powerupAmount++;
+
+            if (powerup == PowerUp.Health)
+            {
+                health += amount;
+                if (health > 100)
+                {
+                    health = 100;
+                }
+            }
+
+            else if (powerup == PowerUp.Shield)
+            {
+                shield += amount;
+                if (shield > 100)
+                {
+                    shield = 100;
+                }
+            }
+        }
+    }
+}
---
+++ GameSixFriday/PowerUp.cs
@@ -0,0 +1,8 @@
+namespace GameSixFriday
+{
+    public enum PowerUp
+    {
+        Health = 1 << 0,
+        Shield = 1 << 1,
+    }
+}
